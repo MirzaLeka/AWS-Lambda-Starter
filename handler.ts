1feb49086fa5b1b00e3a700480d6638407375dc6
mirzaleka <mirza_376@hotmail.com>
2022-12-28 17:56:20 +0100

    Added Github deployment actions

--- handler.ts
+++ handler.ts
@@ -7,6 +7,9 @@ import errorLogger from '@middy/error-logger';
 import createError from 'http-errors';
 
 export const hello = middy(async (event: APIGatewayEvent) => {
+
+  const { DUMMY_API_KEY } = process.env; // Object destructuring
+
   if (event.requestContext.routeKey?.includes('POST')) {
     if (!event.body) {
       throw createError(400, 'Field "body" is required!');
@@ -23,7 +26,7 @@ export const hello = middy(async (event: APIGatewayEvent) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from AWS Lambda with TypeScript on Serverless Framework!',
+      message: `Hello from AWS Lambda with TypeScript on Serverless Framework and this is the DUMMY_API_KEY: >${DUMMY_API_KEY}<`,
     }),
   };
 })
